c28f2670dc6445bc72c856fd2bb72c7aae31cfd5
md omar faruk <hmomarfaruk1454@gmail.com>
2021-08-21 18:03:52 +0600

    update

--- js/app.js
+++ js/app.js
@@ -1,77 +1,65 @@
-function updateTotal() {
-    
+// updating price
+function updatePrice(inputId, cost) {
+    const extraMemory = document.getElementById(inputId, cost);
+    extraMemory.innerText = cost;
+    gettingTotal()
 }
-
-
-
-
-
-
-
-
-/*start mamory-cost */
-// mamory-button-1
-const mamoryButton1 = document.getElementById('mamory-button-1').addEventListener('click', function(){
-    const mamoryCost = document.getElementById('mamory-cost');
-    mamoryCost.innerText = '0';
-    updateTotal()
-});
-// mamory-button-1
-const mamoryButton2 = document.getElementById('mamory-button-2').addEventListener('click', function(){
-    const mamoryCost = document.getElementById('mamory-cost');
-    mamoryCost.innerText = '180';
-    updateTotal()
-});
-/*end mamory-cost 
-*/
-// -------------------------------
-/*
-start storage-cost */
-// storage-button-1
-const storageButton1 = document.getElementById('storage-button-1').addEventListener('click', function(){
-    const storageCost = document.getElementById('storage-cost');
-    storageCost.innerText = '0';
-    updateTotal()
-});
-// storage-button-2
-const storageButton2 = document.getElementById('storage-button-2').addEventListener('click', function(){
-    const storageCost = document.getElementById('storage-cost');
-    storageCost.innerText = '100';
-    updateTotal()
-});
-// storage-button-3
-const storageButton3 = document.getElementById('storage-button-3').addEventListener('click', function(){
-    const storageCost = document.getElementById('storage-cost');
-    storageCost.innerText = '180';
-    updateTotal()
-});
-/*end storage-cost 
-*/
-// -------------------------------
-/*
-start delivery-cost */
-// delivery-button-1
-const deliveryButton1 = document.getElementById('delivery-charge-button-1').addEventListener('click', function(){
-    const deliveryCost = document.getElementById('delivery-charge');
-    deliveryCost.innerText = '0';
-    updateTotal()
-});
-// delivery-button-2
-const deliveryButton2 = document.getElementById('delivery-charge-button-2').addEventListener('click', function(){
-    const deliveryCost = document.getElementById('delivery-charge');
-    deliveryCost.innerText = '20';
-    updateTotal()
-});
-/*end delivery-cost */
-//---------------------------------
-/* add pomo code */
-document.getElementById('code-apply').addEventListener('click', function () {
-    // get user code
-    const codeField = document.getElementById('code-input');
-    const userCode = codeField.value;
-    // check code
-    if (userCode == 'stevekaku') {
-        //console.log('ha paysi');
+// getting total price & final total
+function gettingTotal() {
+    // best price
+    const bestPrice = 1299;
+    // extraMemory total price
+    const extraMemoryTotal = parseFloat(document.getElementById('extra-memory').innerText)
+    // extrastorage total price
+    const extraStorageTotal = parseFloat(document.getElementById('extra-storage').innerText);
+    // delivery cost 
+    const deliveryCostTotal = parseFloat(document.getElementById('delivery-cost').innerText);
+    // getting total price
+    document.getElementById('total-price').innerText = bestPrice + extraMemoryTotal + extraStorageTotal + deliveryCostTotal;
+    // getting total for last total
+    document.getElementById('final-total').innerText = bestPrice + extraMemoryTotal + extraStorageTotal + deliveryCostTotal;
+}
+// final discounted total
+function discountedTotal() {
+    const matchCode = document.getElementById('pomo-input');
+    if (matchCode.value == 'stevekaku') {
+        const TotalPriceText = document.getElementById('total-price');
+        const TotalPrice = parseFloat(TotalPriceText.innerText)
+        const discount = TotalPrice / 5;
+        const discountedTotal = TotalPrice - discount;
+        document.getElementById('final-total').innerText = discountedTotal;
+        matchCode.value = '';
     }
-    codeField.value = '';
-});
+}
+// getting 8gb memory button event-handler
+document.getElementById('button-8gb').addEventListener('click', function () {
+    updatePrice('extra-memory', 0)
+})
+// getting 16gb memory button event-handler
+document.getElementById('button-16gb').addEventListener('click', function () {
+    updatePrice('extra-memory', 180)
+})
+// getting 256gb storage button event-handler
+document.getElementById('button-256gb').addEventListener('click', function () {
+    updatePrice('extra-storage', 0)
+})
+// getting 512gb storage button event-handler
+document.getElementById('button-512gb').addEventListener('click', function () {
+    updatePrice('extra-storage', 100)
+})
+// getting 1TB storage button event-handler
+document.getElementById('button-1tb').addEventListener('click', function () {
+    updatePrice('extra-storage', 180)
+})
+// getting aug21 button event-handler
+document.getElementById('button-aug25').addEventListener('click', function () {
+    updatePrice('delivery-cost', 0)
+})
+// getting aug21 button event-handler
+document.getElementById('button-aug21').addEventListener('click', function () {
+    updatePrice('delivery-cost', 20)
+})
+// apply button event-handler
+document.getElementById('apply-button').addEventListener('click', function () {
+    discountedTotal()
+})
